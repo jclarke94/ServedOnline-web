798b8cb81d1205fa5993142827553820076eed4b
jclarke94 <joel@explodingphone.com>
2019-02-14 17:55:55 +0000

    create new recipe

--- application/controllers/Recipe.php
+++ application/controllers/Recipe.php
@@ -1,9 +1,40 @@
 <?php
 
-efined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Recipe extends SO_Controller {
-	
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('recipe_model');
+    }
+
+    public function createRecipe() {
+    	$postdata = $this->input->post();
+
+    	$this->validateParams($postdata, array(
+    		"userId", 
+    		"recipeTitle", 
+    		"recipeDescription"
+    	));
+
+    	$userId = $postdata["userId"];
+    	$recipeTitle = $postdata["recipeTitle"];
+    	$recipeDescription = $postdata["recipeDescription"];
+    	$timerLength = null;
+    	if (isset($postdata["timerLength"])) {
+    		$timerLength = $postdata["timerLength"];
+    	} 
+    	
+
+    	$result = $this->recipe_model->createNewRecipe($userId, $recipeTitle, $recipeDescription, $timerLength);
+
+    	if ($result) {
+    		$this->JSON(array("success" => TRUE, "data" => $result));
+    	}
+
+    	$this->JSON(array("success" => FALSE, "error" => "could not create new recipe"));
+    }
 }
 
 ?>
--- application/models/Recipe_model.php
+++ application/models/Recipe_model.php
@@ -3,6 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Recipe_model extends SO_Model {
 
+	public function createNewRecipe($userId, $recipeTitle, $recipeDescription, $timerLength = NULL) {
+		$updateData = array(
+			"userId" => $userId, 
+			"recipeTitle" => $recipeTitle,
+			"recipeDescription" => $recipeDescription, 
+			"timerLength" => $timerLength
+		);
+
+		$this->db->insert("recipe", $updateData);
+		$id = $this->db->insert_id();
+    	if ($id > 0) {
+    		return $id;
+   		} else {
+   			return FALSE;
+   		}
+	}
 
 }
 ?>
